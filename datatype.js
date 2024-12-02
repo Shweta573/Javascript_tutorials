86c0622da1d3ca97c6d19b50933bebd142620015
Shweta Shaw <shawshweta71@gmail.com>
2024-12-02 23:17:31 +0530

    Add files via upload
    Here we came to know the different types of datatypes supported in js

---
+++ datatype.js
@@ -0,0 +1,25 @@
+let score = 33
+let score2 = "33abc"
+
+let marks = Number(score2)
+console.log(typeof score)
+console.log( score2)
+console.log( marks)
+
+// 33 --> 33
+// 33abc -->  Nan
+// true --> 1 
+// false --> 0 
+
+let ispass = 1
+let isresult = Boolean(ispass)
+console.log(isresult)
+
+// 1 --> true
+
+let isname = "Shweta"
+let firstname = Boolean(isname)
+console.log(firstname)
+
+// " " --> false
+// "Shweta" --> true
