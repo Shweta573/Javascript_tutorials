19f5cb814dec162a5cfc13a17571e664a2f65b00
Shweta Shaw <shawshweta71@gmail.com>
2024-12-02 23:48:16 +0530

    Add files via upload

---
+++ datatype_summary.js
@@ -0,0 +1,18 @@
+/*
+Types of datatypes:
+Primitive : It includes int,string,boolean,float,double,BigInteger,null,undefined,Symbol. We always use Stack for the same.
+Non primitive: It includes objects, arrays,functions .We always use Heap  for the same
+*/
+let mymarks=100
+const marks1=100.0
+let myEmail="s@example.com"
+isloggedIn=false
+let isCity=null
+let myhobby;
+
+console.log(typeof mymarks) //number
+console.log(typeof marks1) //number
+console.log(typeof myEmail)// string
+console.log(typeof isloggedIn) //boolean
+console.log(typeof isCity) //object
+console.log(typeof myhobby) //undefined
