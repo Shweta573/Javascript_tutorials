e786fb8b8e44586ba5af9adddcbaa73539c89ac2
Shweta Shaw <shawshweta71@gmail.com>
2024-12-02 23:30:31 +0530

    Add files via upload
    Here we deal with different types of datatypes conversion in javascript.

---
+++ datatype_conversion.js
@@ -0,0 +1,24 @@
+// Types Of Opeartions:
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2/2)
+console.log(3 % 2)
+console.log( 2 ** 3)
+
+//String to number conversion:
+console.log("Hello"  +  "World")
+console.log("Hello"  +  1)
+console.log(2 + 1)
+console.log("2" + 1)
+console.log(2 + "1")
+console.log(2 + "1" + 1)
+console.log("2" + "1" + 1)
+console.log(2 + "1" + "1")
+console.log(2 + 1 + "1")
+console.log("2" + 1 + 1)
+
+
+
+
+
