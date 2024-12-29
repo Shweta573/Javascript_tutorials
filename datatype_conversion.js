0c0a964c70b13c97574ffd32126df09b02f49fee
Shweta Shaw <shawshweta71@gmail.com>
2024-12-29 18:35:24 +0530

    Update datatype_conversion.js

--- datatype_conversion.js
+++ datatype_conversion.js
@@ -1,22 +1,22 @@
 // Types Of Opeartions:
-console.log(2+2)
-console.log(2-2)
-console.log(2*2)
-console.log(2/2)
-console.log(3 % 2)
-console.log( 2 ** 3)
+console.log(2+2) // 4
+console.log(2-2) // 0
+console.log(2*2) // 4
+console.log(2/2)// 1
+console.log(3 % 2) // 1
+console.log( 2 ** 3) // 8
 
 //String to number conversion:
-console.log("Hello"  +  "World")
-console.log("Hello"  +  1)
-console.log(2 + 1)
-console.log("2" + 1)
-console.log(2 + "1")
-console.log(2 + "1" + 1)
-console.log("2" + "1" + 1)
-console.log(2 + "1" + "1")
-console.log(2 + 1 + "1")
-console.log("2" + 1 + 1)
+console.log("Hello"  +  "World") // Hello World
+console.log("Hello"  +  1) //  Hello1
+console.log(2 + 1) // 3
+console.log("2" + 1) // 21
+console.log(2 + "1") // 21
+console.log(2 + "1" + 1) // 211
+console.log("2" + "1" + 1) // 211
+console.log(2 + "1" + "1") // 211
+console.log(2 + 1 + "1") // 31
+console.log("2" + 1 + 1) // 211
 
 
 
